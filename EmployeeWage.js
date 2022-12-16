f21907fa0da89e778edceee4aca73a122f743f7d
AswathiMohan23 <achu999here@gmail.com>
2022-12-17 00:02:30 +0530

    [ADDED] : code to find monthly wage assuming no of working days per month as 20

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -9,14 +9,16 @@
         console.log("Employee is Present")
     }
 }
-//UC2
 
+//UC2 and refactored uc2 to do uc3
 
     const IS_PART_TIME=1;
     const IS_FULL_TIME=2;
     const PART_TIME_HOURS=4;
     const FULL_TIME_HOURS=8;
     const WAGE_PER_HOUR=20;
+    const NUM_OF_WORKING_DAYS=2;
+
 
     let empHrs=0;
     empCheck=Math.floor(Math.random()*10)%3;
@@ -28,7 +30,10 @@
             default : return 0;
         }
     }
-    empHrs=getWorkingHours(empCheck);
-    empWage=empHrs*WAGE_PER_HOUR;
-    console.log("employee Wage = "+empWage)
-
+    for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
+        let empCheck=Math.floor(Math.random()*10)%3;
+        empHrs+=getWorkingHours(empCheck);
+    }
+    empHrs=getWorkingHours(empCheck); 
+    empWage = empHrs*WAGE_PER_HOUR;
+    console.log("Total hours : "+empHrs+"\nEmployee Wage : "+empWage);
